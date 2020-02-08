78a7b0f0c3ca5e4e7abb72861214723672afa9d5
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2020-02-09 00:05:14 +0700

    Emotion issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,56 +1,16 @@
-import React from "react";
-import { render } from "react-dom";
-import { Global, css } from "@emotion/core";
-import styled from "@emotion/styled";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { css } from '@emotion/core'
 
-const GlobalStyles = () => (
-  <Global
-    styles={css`
-      body {
-        margin: 0;
-        font-size: 16px;
-        font-family: Arial, Helvetica, sans-serif;
-        background-color: #f5f5f5;
-      }
+const RED = 'red'
 
-      * {
-        box-sizing: border-box;
-      }
+ReactDOM.render(
+  <div
+    css={css`
+      color: ${RED};
     `}
-  />
-);
-
-const HotMessage = styled.div`
-  width: 100%;
-  height: 140px;
-  margin: 0 32px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 4px;
-  font-size: 2rem;
-  background-color: hotpink;
-`;
-
-interface AppProps {
-  foo: number;
-}
-
-const App: React.FC<AppProps> = () => (
-  <React.Fragment>
-    <GlobalStyles />
-
-    <div
-      css={css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        min-height: 100vh;
-      `}
-    >
-      <HotMessage>it works</HotMessage>
-    </div>
-  </React.Fragment>
-);
-
-render(<App foo={123} />, document.getElementById("root"));
+  >
+    Hey
+  </div>,
+  document.getElementById('root')
+)
